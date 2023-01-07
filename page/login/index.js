3049fa92ee5931d78210d70b96906c0d145080a2
DiemQuynhff <quynhltd19406c@st.uel.edu.vn>
2023-01-07 09:23:01 +0700

    abc

---
+++ page/login/index.js
@@ -0,0 +1,2 @@
+const {I} = inject()
+const loginLocator = require('./locator')
---
+++ page/login/locator.js
@@ -0,0 +1,6 @@
+module.exports = {
+    url: 'https://crosstech-sandbox.casso.vn/login',
+    emailField: "#mat-input-0",
+    passwordField: "",
+    loginButton: "",
+}
